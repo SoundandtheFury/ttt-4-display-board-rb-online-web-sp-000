a06e7e4f41e3a31a36fbb3618525ab7d813e3bff
Glenn Jensen <g.e.jensen@gmail.com>
2020-07-29 14:40:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board_entries[" ","X","O"])
+  puts " #{board_entries[0]}  |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board
